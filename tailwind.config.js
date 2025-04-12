7d776a72d82658a6c6598911f84f0f8877440d6c
manishadeepa <manishanarayanasami@gmail.com>
2025-04-12 21:51:01 +0530

    added new files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
-// Placeholder content for tailwind.config.js
+export default{
+    content: ["./index.html", "./src//*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#2563eb', // blue-600
+          accent: '#3b82f6', // blue-500
+        },
+      },
+    },
+    plugins: [],
+  };
