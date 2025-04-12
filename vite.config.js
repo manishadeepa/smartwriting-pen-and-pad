a0e01f0965b43869c4e6968ea4b0e9e2a65e697f
manishadeepa <manishanarayanasami@gmail.com>
2025-04-13 01:58:34 +0530

    added files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-});
+server: {
+  hmr: {
+    overlay: false
+  }
+}
